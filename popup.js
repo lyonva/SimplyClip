fbdf063a9e9640add9f8a1d8725d77b19903b8c8
Leonardo Villalobos Arias <lyon.villalobos@gmail.com>
2021-11-01 21:39:10 -0400

    Restores code I accidentally deleted.

--- popup.js
+++ popup.js
@@ -21,8 +21,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
 let _clipboardList = document.querySelector("#clipboard_list");
 
+
+// Finds all the items in the clipboard Chrome storage and adds them
+// to the list that will be displayed on the UI
 let _flag = 0;
 let search_str = "";
 
@@ -55,7 +59,11 @@ function getClipboardText() {
     });
 }
 
+
+// Displays thumbnail for web links in clipboard
 function getThumbnail(textContent) {
+
+    // Displays thumbnail if URL is a YouTube video
     let ind = textContent.indexOf('https://www.youtube.com/');
     if (ind === 0) {
         let videoId = "";
@@ -73,6 +81,7 @@ function getThumbnail(textContent) {
         };
     }
     else {
+        // Displays thumbnail for all other URL links in the clipboard
         let ind = textContent.indexOf('http');
         if (ind === 0) {
             let url = new URL(textContent);
@@ -99,8 +108,12 @@ function getThumbnail(textContent) {
     }
         ;
 }
+
+// Adds copied items to UI
 function addClipboardListItem(text) {
     let { sourceUrl, imageUrl, isVideo } = getThumbnail(text);
+
+    // Creates HTML elements for each item in the clipboard list
     let listItem = document.createElement("li"),
         listDiv = document.createElement("div"),
         deleteDiv = document.createElement("div"),
@@ -172,6 +185,8 @@ function addClipboardListItem(text) {
     listItem.appendChild(contentDiv);
 
     _clipboardList.appendChild(listItem);
+
+    // Event listener that allows for copied text to be edited
     editImage.addEventListener('click', (event) => {
         console.log("Edit button clicked");
         prevText = listPara.textContent;
@@ -179,6 +194,8 @@ function addClipboardListItem(text) {
         listPara.setAttribute("contenteditable", "true");
         listPara.focus();
     })
+
+    // Event listener that allows for item to be deleted from clipboard UI list
     deleteImage.addEventListener('click', (event) => {
         console.log("Delete clicked");
         chrome.storage.sync.get(['list'], clipboard => {
@@ -190,6 +207,7 @@ function addClipboardListItem(text) {
         })
     })
 
+    // Event listener that allows text to be copied when it is clicked on UI
     listDiv.addEventListener('click', (event) => {
         let { textContent } = event.target;
         convertContentForClipboard(textContent)
@@ -270,10 +288,10 @@ clear_all_btn.addEventListener('click', (event) => {
 )
 
 
-
 // Adds event listener to dark mode toggle button
 document.getElementById("button").addEventListener("click", toggleTheme);
 
+// If the user toggles the theme, the theme becomes the opposite
 function toggleTheme() {
     var theme = document.getElementById('theme');
     chrome.storage.sync.get(['themetoggle'], function (result) {
@@ -302,6 +320,7 @@ function toggleTheme() {
     });
 }
 
+// Gets the theme preference the user has set
 function getTheme() {
     var theme = document.getElementById('theme');
     var button = document.getElementById('button');
@@ -326,5 +345,42 @@ function getTheme() {
     });
 }
 
+// Adds event listener to Save File button
+document.getElementById("savebutton").addEventListener("click", saveClipboardList);
+
+// Saves clipboard list as a csv file
+function saveClipboardList() {
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+    var time = today.getHours().toString() + today.getMinutes().toString() + today.getSeconds().toString();
+    var dateTime = date + ' ' + time;
+    chrome.storage.sync.get(['list'], clipboard => {
+        let list = clipboard.list;
+        let result = "";
+        for (i = 0; i < list.length; i++){
+            result += "\"" + list[i] + "\",\n";
+        }
+        download("Clipboard " + dateTime + ".csv", result);
+    });
+}
+
+// Function that allows all text in clipboard to be saved as a csv file
+// Credit goes to DevonTaig - https://stackoverflow.com/users/1069916/devontaig
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' +
+
+    encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    pom.style.display = 'none';
+    document.body.appendChild(pom);
+
+    pom.click();
+
+    document.body.removeChild(pom);
+}
+
+// Runs startup functions
 getClipboardText();
 getTheme();
