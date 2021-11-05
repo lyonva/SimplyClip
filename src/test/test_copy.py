2b1c645cfb31c20e0adcbae33634f336f292a3af
Connor Smith <cpsmith6@ncsu.edu>
2021-11-04 20:41:02 -0400

    Added multiple tests for copying

--- src/test/test_copy.py
+++ src/test/test_copy.py
@@ -12,19 +12,20 @@ from selenium.webdriver.common.keys import Keys
 
 def test_copy():
     print(sys.path)
-    driver = get_driver()
 
-    assert "hide-div" not in driver.find_element_by_id("empty-div").get_attribute("class")
-    # Navigate to a page and copy text
-    driver.get(f"file://{os.path.dirname(__file__)}/testpage.html")
-    text = driver.find_element_by_id("text1")
-    ActionChains(driver).move_to_element_with_offset(text, 0, 0).click().key_down(
-        Keys.SHIFT
-    ).move_to_element_with_offset(text, 0, 40).click().key_up(Keys.SHIFT).perform()
-    ActionChains(driver).key_down(get_control_key()).send_keys("c").key_up(get_control_key()).perform()
-    time.sleep(2)
+    for i in range(5):
+        driver = get_driver()
+        assert "hide-div" not in driver.find_element_by_id("empty-div").get_attribute("class")
+        # Navigate to a page and copy text
+        driver.get(f"file://{os.path.dirname(__file__)}/testpage.html")
+        text = driver.find_element_by_id("text1")
+        ActionChains(driver).move_to_element_with_offset(text, 0, 0).click().key_down(
+            Keys.SHIFT
+        ).move_to_element_with_offset(text, 0, 40).click().key_up(Keys.SHIFT).perform()
+        ActionChains(driver).key_down(get_control_key()).send_keys("c").key_up(get_control_key()).perform()
+        # time.sleep(2)
 
-    # Go pack to the page
-    driver.get(app_popup_page)
-    time.sleep(2)
-    assert "hide-div" in driver.find_element_by_id("empty-div").get_attribute("class")
+        # Go pack to the page
+        driver.get(app_popup_page)
+        # time.sleep(2)
+        assert "hide-div" in driver.find_element_by_id("empty-div").get_attribute("class")
